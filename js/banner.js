3020bcfceb84d15b26a1e60c16be66180751d73d
cdujardin4000 <51912624+cdujardin4000@users.noreply.github.com>
2021-12-02 02:29:39 +0100

    pic

--- js/banner.js
+++ js/banner.js
@@ -10,9 +10,9 @@ $(function(){
     let slide1 = document.createElement('div');
     slide1.className = "slide slide1";
     
-    let caption = document.createElement('div');
-    caption.className = "caption";
-    slide1.append(caption);
+    //let caption = document.createElement('div');
+    //caption.className = "caption";
+    //slide1.append(caption);
     slider.append(slide1);
 
     let slide2 = document.createElement('div');
@@ -39,10 +39,19 @@ $(function(){
     //slide3.append(caption3);
     slider.append(slide4);
 
+    let slide5 = document.createElement('div');
+    slide5.className = "slide slide5";
+
+    //let caption3 = document.createElement('div');
+    //caption3.className = "caption";
+    //slide3.append(caption3);
+    slider.append(slide5);
+
     console.log(slider);
     TARGETSLIDER.append(slider);
-    $('.slider, .sliderTarget, .slide.slide1').css({'background': `url("./img/synthesizer-2427767_1920.jpg")`, 'background-size' : 'cover', 'background-position': 'center'});
-    $('.slider, .sliderTarget, .slide.slide2').css({'background': 'url("./img/synthesizer-908298_1920.jpg")', 'background-size' : 'cover', 'background-position': 'center'});
-    $('.slider, .sliderTarget, .slide.slide3').css({'background': 'url("./img/synthesizer-2479008_1920.jpg")', 'background-size' : 'cover', 'background-position': 'center'});
-    $('.slider, .sliderTarget, .slide.slide4').css({'background': 'url("./img/synthesizer-2814623_1920.jpg")', 'background-size' : 'cover', 'background-position': 'center'});
+    $('.slider, .sliderTarget, .slide.slide1').css({'background': `url("./img/synthesizer-2427767_1920.jpg")`, 'background-size' : '100vw , 50%', 'background-position': 'center'});
+    $('.slider, .sliderTarget, .slide.slide2').css({'background': 'url("./img/synthesizer-908298_1920.jpg")', 'background-size' : '100vw , 50%', 'background-position': 'center'});
+    $('.slider, .sliderTarget, .slide.slide3').css({'background': 'url("./img/synthesizer-2479008_1920.jpg")', 'background-size' : '100vw , 50%', 'background-position': '100% 100%'});
+    $('.slider, .sliderTarget, .slide.slide4').css({'background': 'url("./img/synthesizer-2814623_1920.jpg")', 'background-size' : '100vw , 50%', 'background-position': '50% 200%'});
+    $('.slider, .sliderTarget, .slide.slide5').css({'background': 'url("./img/moog-1573329_1920.jpg")', 'background-size' : '100vw , 50%', 'background-position': '100% 0%'});
 });
